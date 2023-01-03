7d0661c31c909546d4f4bb1252c6394de04459b1
Patrick <kelly.patj@gmail.com>
2023-01-03 17:22:47 -0500

    finishes functionality

--- data/bikes.js
+++ data/bikes.js
@@ -1,5 +1,5 @@
 const bikes = [
-  {year: 1982, make: 'Yamaha', model: 'Virago', runs: false, dis: '920cc'},
+  {year: 1982, make: 'Yamaha', model: 'Virago', runs: true, dis: '920cc'},
   {year: 1974, make: 'Honda', model: 'CB750', runs: false, dis: '749cc'},
   {year: 2003, make: 'Yamaha', model: 'V-Strom', runs: true, dis: '1000cc'},
   {year: 2002, make: 'White Knuckle Cycles', model: "Misery's Company", runs: false, dis:'124ci'}
